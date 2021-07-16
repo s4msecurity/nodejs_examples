cbe8f400abe00daff7705e9aeb106bede316f6bd
s4msecurity <81925511+s4msecurity@users.noreply.github.com>
2021-07-16 14:33:43 +0300

    Add files via upload

---
+++ postnodeJS-main/app.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const app = express()
+const path = require("path")
+const bodyParser = require("body-parser")
+
+app.use(bodyParser.urlencoded({ extended: false })) //Genel olarak sayfa işleyişi ayarlandı.
+//app.use(express.json)
+app.use(express.static(path.join(__dirname, "img"))) //resimleri erişilebilir
+
+app.set("view engine", "pug") //pug view motoru aktifleştirildi.
+app.set("views", "./views") //views olarak klasör olması gerekli.
+
+const indexPage = require("./router/index") //index dosyasında JS özelliğini aldık.
+const admin = require("./router/addStock") //admin JS dsyasını özelliğini aldık
+app.use(indexPage) //indexJS eklendi.
+app.use(admin) //admin diye addstock js eklendi.
+
+app.use(function (req, res, next) {
+    res.status(404).render("404") //render pug ile gelen bir özellik direk view içinde dosyanın adını alarak okuyor.
+})
+
+app.listen(3000, function () {
+    console.log("Server running")
+})
+
---
+++ postnodeJS-main/readme.txt
@@ -0,0 +1,4 @@
+Sayfalar arası geçiş.
+PUG kullanımı
+POST ile array içine değer atma
+Ve listeleme işlemleri
